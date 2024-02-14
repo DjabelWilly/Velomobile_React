11b2996a10c600692b1ae00ca2484c49bfbba561
will <djabel.willyu@gmail.com>
2024-02-14 11:43:49 +0100

    update form

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ handleClick }) => {
+  return <button onClick={handleClick} className="btn btn-primary">Envoyé</button>;
+};
+
+export default Button;
---
+++ src/components/FormContact.jsx
@@ -0,0 +1,21 @@
+import { Button } from "bootstrap";
+
+const FormContact = () => {
+  return (
+    <div className="container-form my-5">
+      <form
+        id="form-contact"
+        className="d-flex flex-column d-inline-flex col-md-3">
+        <label htmlFor="lastname">Prénom</label>
+        <input type="text" id="lastname" />
+        <label htmlFor="firstname">Nom</label>
+        <input type="text" id="firstname" />
+        <label htmlFor="textarea">Votre message</label>
+        <textarea id="message" name="" rows="6"></textarea>
+       
+      </form>
+    </div>
+  );
+};
+
+export default FormContact;
